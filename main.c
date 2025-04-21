8193332de1ddc3d243ca736ef39b296063d55511
Hippopcorn <elsa.varache@gmail.com>
2025-04-21 14:56:05 +0200

    read input

--- main.c
+++ main.c
@@ -1,59 +1,91 @@
+#include <stdio.h>
 #include <unistd.h>
-
 
 void ft_putchar(char c);
 void ft_full_line(int size);
 void ft_empty_line(int size);
 void ft_create_square(int size);
+int ft_read_enter(char enter[]);
+int convert_char_to_int(char enter[], int sizeString);
 
 int main() {
-	int squareSize = 2;
-	ft_create_square(squareSize);
-		return 0;
+    char enterSquareSize[10];
+    int sizeEnter = ft_read_enter(enterSquareSize);
+    int squareSize = convert_char_to_int(enterSquareSize, sizeEnter);
+
+    ft_create_square(squareSize);
+    return 0;
 }
 
-
 void ft_putchar(char c) {
-	write(1, &c, 1);
+    write(1, &c, 1);
 }
 
 void ft_full_line(int size) {
-	int i = 0;
-	while (i < size) {
-		ft_putchar('*');
-		i++;
-	}
-	ft_putchar('\n');
+    int i = 0;
+    while (i < size) {
+        ft_putchar('*');
+        i++;
+    }
+    ft_putchar('\n');
 }
 
 void ft_empty_line(int size) {
-	int i = 0;
-	ft_putchar('*');
-	while (i < (size - 2)) {
-		ft_putchar(' ');
-		i++;
-	}
-	ft_putchar('*');
-	ft_putchar('\n');
+    int i = 0;
+    ft_putchar('*');
+    while (i < (size - 2)) {
+        ft_putchar(' ');
+        i++;
+    }
+    ft_putchar('*');
+    ft_putchar('\n');
 }
 
 void ft_create_square(int size) {
+    if (size == 1) {
+        ft_full_line(size);
+        return;
+    }
 
-		if (size == 1) {
-			ft_full_line(size);
-		}
+    int nbr_empty_line = size - 2;
+    int counter = 0;
+    ft_full_line(size);
 
-		else {
-	      	 int nbr_empty_line = size - 2;	        
-    		 int counter = 0; 
-         	 ft_full_line(size);
+    while (counter < nbr_empty_line) {
+        ft_empty_line(size);
+        counter++;
+    }
 
-         	 while (counter < nbr_empty_line) {
-                  ft_empty_line(size);
-                  counter++;
-         	 }
-
-         	 ft_full_line(size);
-	}	
+    ft_full_line(size);
 }
-			
+
+int ft_read_enter(char enter[]) { // doit renvoyer la taille du tableau
+    int i = 0;
+    while (i < 10) {
+        read(1, &enter[i], 1);
+        if (enter[i] == '\n') {
+            return i;
+        }
+        ft_putchar(enter[i]);
+        i++;
+    }
+
+    return i;
+}
+
+int convert_char_to_int(char enter[], int sizeString) {
+    int iChar = sizeString - 1;
+    int multiplicationFactor = 1;
+    int squareSize = 0;
+
+    while (iChar >= 0) {
+        squareSize += (enter[iChar] - '0') * multiplicationFactor;
+        printf("i = %d, enter[i] = %c\n", iChar, enter[iChar]);
+
+        printf("squareSize = %d\n", squareSize);
+        multiplicationFactor *= 10;
+        iChar--;
+    }
+    printf("squareSize = %d\n", squareSize);
+    return squareSize;
+}
